695e63f07438a4984a77b92c2b5eab980139267e
Mghempire <87279547+Mghempire@users.noreply.github.com>
2022-07-28 14:38:38 +0300

    Kek

---
+++ Assets/Scripts/CAMMOVE.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CAMMOVE : MonoBehaviour {
+    public GameObject obj;
+    public float speed = 2;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 newpos = obj.transform.position;
+            newpos.x += speed;
+            obj.transform.position = newpos;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 newpos = obj.transform.position;
+            newpos.x -= speed;
+            obj.transform.position = newpos;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 newpos = obj.transform.position;
+            newpos.y += speed;
+            obj.transform.position = newpos;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 newpos = obj.transform.position;
+            newpos.y -= speed;
+            obj.transform.position = newpos;
+        }
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+        {
+            Vector3 newpos = obj.transform.position;
+            newpos.x += speed/2;
+            newpos.y += speed/2;
+            obj.transform.position = newpos;
+        }
+        else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        {
+            Vector3 newpos = obj.transform.position;
+            newpos.x -= speed/2;
+            newpos.y += speed/2;
+            obj.transform.position = newpos;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            Vector3 newpos = obj.transform.position;
+            newpos.y -= speed/2;
+            newpos.x += speed/2;
+            obj.transform.position = newpos;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            Vector3 newpos = obj.transform.position;
+            newpos.y -= speed/2;
+            newpos.x -= speed/2;
+            obj.transform.position = newpos;
+        }
+    }
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public GameObject Unit;
+    public Vector3 MouseTouchPoint;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Camera.main.ScreenPointToRay(MouseTouchPoint);
+            Unit.transform.position = MouseTouchPoint;
+        }
+	}
+}
